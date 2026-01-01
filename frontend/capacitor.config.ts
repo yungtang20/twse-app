4a8ba9915a83610fc433d6f63a80d005edd861bd
詠棠 <yungtang2011@gmail.com>
2026-01-01 14:19:35 +0800

    fix: enable cleartext traffic and set android scheme to https

--- frontend/capacitor.config.ts
+++ frontend/capacitor.config.ts
@@ -3,7 +3,11 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.twse.app',
   appName: 'TWSE',
-  webDir: 'dist'
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https',
+    cleartext: true
+  }
 };
 
 export default config;
